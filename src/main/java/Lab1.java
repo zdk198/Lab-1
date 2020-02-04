1e1b437d17db057af60df4cb07f2801f47f702ce
zachkorte <zachkorte@dhcp-172-16-251-100.dyn.pittstate.edu>
2020-02-04 17:17:55 -0600

    Did Sales Tax Problem

--- src/main/java/Lab1.java
+++ src/main/java/Lab1.java
@@ -1,13 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author zachkorte
- */
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+//Zach Korte
 public class Lab1 {
+    //Sales Tax Question
+    public static void main(String[] args) { 
+        Scanner keyboard = new Scanner(System.in);
+         String AmountOfPurchase = JOptionPane.showInputDialog("Please input purchase price.");
+         double CountyTaxRate = .02 ;
+         double StateTaxRate= .04; 
+         double CountyTotalTax = Double.parseDouble(AmountOfPurchase)*  CountyTaxRate ;
+         double StateTotalTax = Double.parseDouble(AmountOfPurchase)* StateTaxRate;
+         double TotalTaxCharged = StateTotalTax + CountyTotalTax ;
+         double TotalSalePrice = Double.parseDouble(AmountOfPurchase)+ TotalTaxCharged;
+         JOptionPane.showMessageDialog(null, "The amount of the purchase is  " + AmountOfPurchase + "\n The State Sales Tax is $" + StateTotalTax + "\n The County Sales Tax is $" + CountyTotalTax + "\n The Total Tax Charged is $" + TotalTaxCharged + "\n The Total Sale Price is $" + TotalSalePrice );
+              
+    }
     
 }
